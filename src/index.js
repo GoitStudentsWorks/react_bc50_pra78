42a6eae9e13dc39adac355af323721d41d5fe62d
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-06-01 14:15:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-bc50-react-project">
+        <BrowserRouter basename="/react_bc50_pra78">
           <Auth>
             <App />
           </Auth>
